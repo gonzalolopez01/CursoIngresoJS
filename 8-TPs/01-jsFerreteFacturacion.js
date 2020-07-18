522d69396776dc23044505abebf1595b3550aa11
gonzalolopez01 <68036425+gonzalolopez01@users.noreply.github.com>
2020-07-18 20:35:18 -0300

    Add files via upload
    18/07

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -4,14 +4,68 @@ B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
-{
-	
+{   
+    let precio1;
+    let precio2;
+    let precio3;
+    let suma;
+
+
+    precio1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+
+    precio2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+
+    precio3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+
+    suma = precio1 + precio2 + precio3;
+
+    alert ("La suma total es $" + suma);
+
 }
 function Promedio () 
 {
-	
+	let precio1;
+    let precio2;
+    let precio3;
+    let promedio;
+
+
+    precio1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+
+    precio2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+
+    precio3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+
+    promedio = (precio1 + precio2 + precio3) / 3;
+
+    alert ("El promedio es $" + promedio.toFixed(2));
+
 }
 function PrecioFinal () 
 {
-	
+	let precio1;
+    let precio2;
+    let precio3;
+    let suma;
+    let IVA;
+    let precioConIVA;
+
+
+    precio1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+
+    precio2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+
+    precio3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+
+    
+    suma = precio1 + precio2 + precio3;
+
+    IVA = suma * 21 / 100;
+
+    precioConIVA = suma + IVA;
+
+    //precioConIVA = precioConIVA.toFixed(2); se puede hacer para ya asignarle a la avariable el total con solo 2 decimales.
+
+    alert ("El total con IVA es $" + precioConIVA.toFixed(2));
+
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,59 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    let largo;
+    let ancho;
+    let perimetro;
+    let alambre;
+    
+    largo = parseFloat(document.getElementById("txtIdLargo").value);
+    
+    ancho = parseFloat(document.getElementById("txtIdAncho").value);
+   
+    perimetro = 2 * (largo + ancho);
 
+    alambre = perimetro * 3; // porque son 3 vueltas de alambre
+    
+    alert("La cantidad de alambre a comprar es " + alambre + " metros de alambre.");
 }
 function Circulo () 
 {
+    let radio;
+    let perimentro;
+    let alambre;
+    const PI = 3.14; //constantes van con mayúsculas y se les asigna el valor en la misma linea en la que se declara
+    //o const PI = Math.PI;
+    
+
+    radio = parseFloat(document.getElementById("txtIdRadio").value);
+
+    perimetro = 2 * PI * radio;
+
+    alambre = perimetro * 3;
+
+    alert("La cantidad de alambre a comprar es " + alambre.toFixed(2) + " metros de alambre.");
 	
 }
 function Materiales () 
 {
-	
+	let largo;
+    let ancho;
+    let superficie;
+    let Cal;
+    let Cemento;
+    const CEMENTO_X_METRO = 2;
+    const CAL_X_METRO = 3;
+    
+    largo = parseFloat(document.getElementById("txtIdLargo").value);
+    
+    ancho = parseFloat(document.getElementById("txtIdAncho").value);
+   
+    superficie = largo * ancho;
+
+    Cemento = superficie * CEMENTO_X_METRO;
+
+    Cal = superficie * CAL_X_METRO;
+    
+    alert(`Para un contra piso de una superficie de ${superficie} m2 se debe comprar ${Cemento} bolsas de cemento y ${Cal} bolsas de Cal.`);
+//hola
 }
